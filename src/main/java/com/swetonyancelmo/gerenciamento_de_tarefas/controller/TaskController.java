f60b6e7e0ee7b6e2f28a86b508288d08a580a47b
Swetony <swetonyancelmo@gmail.com>
2025-10-13 16:30:44 -0300

    refactor: Refatorando controller para usar a service

--- src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/controller/TaskController.java
+++ src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/controller/TaskController.java
@@ -4,90 +4,87 @@ import com.swetonyancelmo.gerenciamento_de_tarefas.dtos.AtualizarTaskRequestDTO;
 import com.swetonyancelmo.gerenciamento_de_tarefas.dtos.CriarTaskRequestDTO;
 import com.swetonyancelmo.gerenciamento_de_tarefas.dtos.TaskResponseDTO;
 import com.swetonyancelmo.gerenciamento_de_tarefas.models.Task;
+import com.swetonyancelmo.gerenciamento_de_tarefas.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
-    // Lista de Tarefas (simula um banco de dados)
-    List<Task> listaDeTarefas = new ArrayList<>();
-    // Usado para autoincrementar os ids
-    private final AtomicLong contadorDeId = new AtomicLong();
+    @Autowired
+    private TaskService taskService;
 
-    // Retorna todas as tarefas da lista
+    private TaskResponseDTO convertToResponseDTO(Task task) {
+        return new TaskResponseDTO(
+                task.getId(),
+                task.getTitulo(),
+                task.getDescricao(),
+                task.isConcluida()
+        );
+    }
+
     @GetMapping
     public ResponseEntity<List<TaskResponseDTO>> listarTodasTarefas() {
-        List<TaskResponseDTO> dtos = listaDeTarefas.stream()
-                .map(task -> new TaskResponseDTO(task))
-                .toList();
+        List<Task> tasks = taskService.listarTodas();
+        List<TaskResponseDTO> dtos = tasks.stream()
+                .map(TaskResponseDTO::new)
+                .collect(Collectors.toList());
         return ResponseEntity.ok(dtos);
     }
 
-    // Retorna uma tarefa específica por id
     @GetMapping("/{id}")
-    public ResponseEntity<TaskResponseDTO> buscarTarefaPorId(@PathVariable long id) {
-        return listaDeTarefas.stream()
-                .filter(tarefa -> tarefa.getId().equals(id))
-                .findFirst()
-                .map(tarefaEncontrada -> ResponseEntity.ok(new TaskResponseDTO(tarefaEncontrada)))
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<TaskResponseDTO> buscarTarefaPorId(@PathVariable Long id) {
+        try {
+            Task task = taskService.buscarPorId(id);
+            return ResponseEntity.ok(new TaskResponseDTO(task));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
     }
 
-    // Cria uma nova tarefa
     @PostMapping
     public ResponseEntity<TaskResponseDTO> criarTarefa(@RequestBody CriarTaskRequestDTO dto) {
-        Task task = new Task();
-        task.setId(contadorDeId.incrementAndGet());
-        task.setTitulo(dto.getTitulo());
-        task.setDescricao(dto.getDescricao());
-
-        listaDeTarefas.add(task);
-
-        return new ResponseEntity<>(new TaskResponseDTO(task), HttpStatus.CREATED);
+        Task novaTarefa = taskService.criarTarefa(dto);
+        return new ResponseEntity<>(convertToResponseDTO(novaTarefa), HttpStatus.CREATED);
     }
 
-    // Atualiza todos os campos de uma tarefa específica
     @PutMapping("/{id}")
     public ResponseEntity<TaskResponseDTO> atualizarTarefa(@PathVariable long id,
                                                            @RequestBody AtualizarTaskRequestDTO dto) {
-        for (Task task : listaDeTarefas) {
-            if (task.getId().equals(id)) {
-                task.setTitulo(dto.getTitulo());
-                task.setDescricao(dto.getDescricao());
-                task.setConcluida(dto.isConcluida());
-                return ResponseEntity.ok(new TaskResponseDTO(task));
-            }
+        try {
+            Task task = new Task();
+            task.setTitulo(dto.getTitulo());
+            task.setDescricao(dto.getDescricao());
+            task.setConcluida(dto.isConcluida());
+
+            Task atualizado = taskService.atualizarTarefa(id, task);
+            return ResponseEntity.ok(new TaskResponseDTO(atualizado));
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
         }
-        return ResponseEntity.notFound().build();
     }
 
-    // Atualiza o campo concluido para true
     @PatchMapping("/{id}/concluir")
     public ResponseEntity<TaskResponseDTO> concluirTarefa(@PathVariable long id) {
-        for (Task task : listaDeTarefas) {
-            if (task.getId().equals(id)) {
-                task.setConcluida(true);
-                return ResponseEntity.ok(new TaskResponseDTO(task));
-            }
+        try {
+            Task concluido = taskService.concluirTarefa(id);
+            return ResponseEntity.ok(new TaskResponseDTO(concluido));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
         }
-        return ResponseEntity.notFound().build();
     }
 
-    // Exclui uma tarefa por id
     @DeleteMapping("/{id}")
     public ResponseEntity<TaskResponseDTO> deletarTarefa(@PathVariable long id) {
-        boolean removido = listaDeTarefas.removeIf(tarefa -> tarefa.getId().equals(id));
-        if (removido) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
+        taskService.deletarPorId(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
