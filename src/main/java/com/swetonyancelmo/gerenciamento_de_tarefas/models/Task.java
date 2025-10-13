5827e03881a4d7bae8d1cc98fb5fe993688797ed
Swetony <swetonyancelmo@gmail.com>
2025-10-13 16:29:35 -0300

    feat: Transforma classe em entidade no banco de dados

--- src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/models/Task.java
+++ src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/models/Task.java
@@ -1,16 +1,24 @@
 package com.swetonyancelmo.gerenciamento_de_tarefas.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "tarefas")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Task {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String titulo;
+
     private String descricao;
-    private boolean concluida = false;
+
+    private boolean concluida;
 }
