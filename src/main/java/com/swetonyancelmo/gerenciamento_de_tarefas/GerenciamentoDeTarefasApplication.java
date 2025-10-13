99b493974ffa881979d162acce18fd63cd4b508e
Swetony <swetonyancelmo@gmail.com>
2025-10-13 10:51:25 -0300

    Adiciona arquivos necessários

---
+++ src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/GerenciamentoDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo.gerenciamento_de_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swetonyancelmo/gerenciamento_de_tarefas/GerenciamentoDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo.gerenciamento_de_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
