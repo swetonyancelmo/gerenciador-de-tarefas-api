197214e54600aaec737d0b638ede5eb9bac9ba76
Swetony <swetonyancelmo@gmail.com>
2025-10-13 16:31:44 -0300

    feat: Adiciona camada de exceções

---
+++ src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.swetonyancelmo.gerenciamento_de_tarefas.exceptions;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String message){
+        super(message);
+    }
+}
