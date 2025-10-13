892489b79d37640966be2bc6d386174abb5f662a
Swetony <swetonyancelmo@gmail.com>
2025-10-13 16:27:54 -0300

    feat: Adiciona camada de repository

---
+++ src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.swetonyancelmo.gerenciamento_de_tarefas.repositories;
+
+import com.swetonyancelmo.gerenciamento_de_tarefas.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
