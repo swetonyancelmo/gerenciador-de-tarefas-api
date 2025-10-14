99b0927d884cb778bcbfc5742b7aa233a3cd9969
Swetony <swetonyancelmo@gmail.com>
2025-10-14 20:40:26 -0300

    refactor: Adiciona Validation

--- src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/dtos/CriarTaskRequestDTO.java
+++ src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/dtos/CriarTaskRequestDTO.java
@@ -1,5 +1,7 @@
 package com.swetonyancelmo.gerenciamento_de_tarefas.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +9,10 @@ import lombok.Setter;
 @Setter
 public class CriarTaskRequestDTO {
 
+    @NotBlank(message = "O título não pode estar em branco.")
+    @Size(max = 100, message = "O título não pode ter mais que 100 caracteres.")
     private String titulo;
+
+    @Size(max = 255, message = "A descrição não pode ter mais que 255 caracteres.")
     private String descricao;
 }
