f41b8fada6f182cef7f294cda5e117c466621775
Swetony <swetonyancelmo@gmail.com>
2025-10-15 16:03:13 -0300

    feat: Adiciona repository de User

---
+++ src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.swetonyancelmo.gerenciamento_de_tarefas.repositories;
+
+import com.swetonyancelmo.gerenciamento_de_tarefas.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
