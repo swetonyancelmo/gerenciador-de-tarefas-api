f3d05087527ddc6ea0846b814e2574343a505e75
Swetony <swetonyancelmo@gmail.com>
2025-10-14 21:10:44 -0300

    feat: Adiciona modelo de classe Usuario

---
+++ src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/models/User.java
@@ -0,0 +1,25 @@
+package com.swetonyancelmo.gerenciamento_de_tarefas.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+}
