8f8e273a9026910adaaa7d6f8b4aa1c3511ef3e5
Swetony <swetonyancelmo@gmail.com>
2025-10-15 20:38:02 -0300

    feat: Adiciona exceção personalizada de Email

---
+++ src/main/java/com/swetonyancelmo/gerenciamento_de_tarefas/exceptions/EmailJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.swetonyancelmo.gerenciamento_de_tarefas.exceptions;
+
+public class EmailJaCadastradoException extends RuntimeException{
+    public EmailJaCadastradoException(String message){
+        super(message);
+    }
+}
